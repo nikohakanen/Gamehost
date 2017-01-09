68635c0a77e91029275df765e74a8b984b1c09c7
Santeri Mahlamäki <santeri.mahlamaki@aalto.fi>
2017-01-09 20:39:37 +0200

    Added functionality to the models. Testing still in progress

--- gamehost/models.py
+++ gamehost/models.py
@@ -3,21 +3,30 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class SiteUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     developer_status = models.BooleanField(default=False)
 
+
 class Game(models.Model):
     category = models.TextField()
+    developer = models.OneToOneField(SiteUser, on_delete=models.CASCADE)
+
 
 class Highscore(models.Model):
-    pass
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    player = models.ForeignKey(SiteUser, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
+
 
 class Transaction(models.Model):
-    pass
+    player = models.ForeignKey(SiteUser)
+    game = models.ForeignKey(Game)
+    date = models.DateField(auto_now=False, auto_now_add=True)
+
 
 class Savedata(models.Model):
-    pass
-
-
-    
+    player = models.ForeignKey(SiteUser, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    data = models.TextField()
