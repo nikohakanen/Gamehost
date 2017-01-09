57f991200af4c73e3f30e3fbb52e2012067177e6
Santeri Mahlamäki <santeri.mahlamaki@aalto.fi>
2017-01-09 15:27:46 +0200

    Added stubs for rest of the expected models

--- gamehost/models.py
+++ gamehost/models.py
@@ -6,3 +6,18 @@ from django.contrib.auth.models import User
 class SiteUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     developer_status = models.BooleanField(default=False)
+
+class Game(models.Model):
+    category = models.TextField()
+
+class Highscore(models.Model):
+    pass
+
+class Transaction(models.Model):
+    pass
+
+class Savedata(models.Model):
+    pass
+
+
+    
