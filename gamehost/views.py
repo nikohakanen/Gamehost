f5a7c518d27f10e6b0acd198c8f92755376a6256
Niko Hakanen <hakanen1@aalto.fi>
2017-01-16 14:37:40 +0200

    Added 2 templates base and home. Homeview can now show all games. Added stylesheet.

--- gamehost/views.py
+++ gamehost/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from gamehost.models import Game
 
 # Create your views here.
 
 
 # Just to test how the views worked again.
 def homeview(request):
-    return render(request, 'base.html')
+
+    game_list = Game.objects.all()
+
+    return render(request, 'home.html', {'games': game_list})
